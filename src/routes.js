9a59c53f4e84fe3f4c95df0e5ae70dbeef42b0c7
Léu Almeida <leo@webid.net.br>
2020-04-01 16:53:38 -0300

    fix(routes): improving post and get methods

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
-
-//código criado para visualização com o isominia para visulização e gerenciamento de 
+//código criado para visualização com o isominia para visulização e gerenciamento de
 //código e autenticação. No exemplo abaixo o insomina retorna dados do cliente
 //criação de uma rota users
 
@@ -24,92 +23,79 @@ routes.post('/users', (request, response) =>{
 
  */
 
-
-
-
-
-
-
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
-const crypto = require('crypto');
+const crypto = require("crypto");
 //crypto é um pacote q vem junto com o node, nessa var eu importei ele
 //com um método dele consigo gerar um texto aleatório ou uma string aleatória
 //método usado -> randomBytes
 //random = aleatório
 //ele vai ser usado pra criar uma id pra cada ong que acessar o site
-const connection = require('./database/connection');
+const connection = require("./database/connection");
 //importação da pasta connection para comunicação com o banco de dados
 //agora pode ser feito as operações com o banco de dados
 
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
+  //connection na tabela ongs, selecione todos os registros q tem na tabela ongs
+  // o asterisco quer dizer isso
 
-
-
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-    //connection na tabela ongs, selecione todos os registros q tem na tabela ongs
-    // o asterisco quer dizer isso
-
-    return response.json(ongs);
+  return response.json(ongs);
 });
 
 //rota criada para cadastrar as ids das ongs
 //temos duas rotas iguais com métodos diferente, uma rota é get e outra é post
 
+routes.post("/ongs", async (request, response) => {
+  //const data = request.body;
+  // dessa forma a variável data guarda todas as informações
+  //função definida como assícrona por async
 
-
-
-routes.post('/ongs', (request, response) =>{
-    //const data = request.body;
-    // dessa forma a variável data guarda todas as informações
-    //função definida como assícrona por async
-
-  const { name, email, whatsapp, city, uf} = request.body; 
+  const { name, email, whatsapp, city, uf } = request.body;
   //dessa forma eu garanto q só terão esses itens e evito q o usuário prrencha algum campo q eu não queira
 
-    //console.log(data); //apenas pra visualizção no terminal
-     
-    const id = crypto.randomBytes(4).toString('HEX');
-    //número 4 siginifica q ele vai gerar 4 numéros aleatórios para formar
-    //a id de cada ong.
+  //console.log(data); //apenas pra visualizção no terminal
 
-     connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    });
+  const id = crypto.randomBytes(4).toString("HEX");
+  //número 4 siginifica q ele vai gerar 4 numéros aleatórios para formar
+  //a id de cada ong.
 
-    /**
-     * Especificações sobre: await connection('ongs').insert({
-     *   aqui dentro fica todas as colunas q eu quero inseri ali dentro
-     * ex.: id, email, nome, whatsap, city, uf..
-     * 
-     * });
-     * 
-     * await = só pode ser usado se a função for assícrona(async),
-     * quando o node chegar no código com o await ele irá aguardar, que é jusatamente
-     * a tradução dessa palavra (await = aguardar) então ele só continuára depois q finalizar
-     * o código. 
-     * 
-     * Utilizamos a var connection e passamos o parametro dela de 'ongs' q é a tabela q e
-     * eu quero inserir dados, utilizamos o método insert para inserir dados ali dentro
-     * 
-     */
-
-    return response.json({ id });
-    
-  } ); 
-  //reposta ao usuário, coloque a reposta dentro do parãmetro com {}
-  //a reposta será uma id ao usuário, essa id servirá de cadastro para a ong se cadastrar
-  //como que fosse um cpf da ong
-
-  module.exports = routes;
-  //exportando o conteúdo de routes
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
 
   /**
+   * Especificações sobre: await connection('ongs').insert({
+   *   aqui dentro fica todas as colunas q eu quero inseri ali dentro
+   * ex.: id, email, nome, whatsap, city, uf..
+   *
+   * });
+   *
+   * await = só pode ser usado se a função for assícrona(async),
+   * quando o node chegar no código com o await ele irá aguardar, que é jusatamente
+   * a tradução dessa palavra (await = aguardar) então ele só continuára depois q finalizar
+   * o código.
+   *
+   * Utilizamos a var connection e passamos o parametro dela de 'ongs' q é a tabela q e
+   * eu quero inserir dados, utilizamos o método insert para inserir dados ali dentro
+   *
+   */
+
+  return response.json({ id });
+});
+//reposta ao usuário, coloque a reposta dentro do parãmetro com {}
+//a reposta será uma id ao usuário, essa id servirá de cadastro para a ong se cadastrar
+//como que fosse um cpf da ong
+
+module.exports = routes;
+//exportando o conteúdo de routes
+
+/**
    * Erro ao retornar reposta ao usuário, a reposta que seria a id do cliente
    * quando a resposta não for criada mas mesmo assim paracer a inserção dos meus dados
    * no terminal e parecer um erro dizendo: no such table ongs, quer dizer q ele não a tabela ongs
