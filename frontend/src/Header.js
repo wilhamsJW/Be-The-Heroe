1268193c4e1ba99306ff167ae0b29ca510cae528
wilhamsJW <wilhamsmeira@gmail.com>
2020-04-04 20:11:14 -0300

    initial front end

---
+++ frontend/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    
+    <header>
+      <h1>Wilhams Developer</h1>
+    </header>
+ 
+  );
+}
+
+//export default Header;
+//exportando o Header para onde ele quiser ir
+//pode se usar o export defaul antes da function ou uma linha aqui em baixo de quiser
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+//importing react
 import ReactDOM from 'react-dom';
-import './index.css';
+//importing DOM, é a integração do react com o DOM, o DOM é arvore de elementos do navegador
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//importing app, esse app é a pasta App.js
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
+render = renderizar = obter o produto final / na web = colocar em tela
+ou seja ele vai colocar em tela o App (App é um componente do react, geralmente
+tem letras miasculas) q lá me trás código, esse App tá sendo colocado dentro da div root
+que é a div da meu index.html.
+
+Um componente no react é uma função que me retorna html ou css
+
+Porém quando um arquivo html está dentro de um arquivo javascript isso é chamado de JSX
+Java Script Xml = JSX
+*/
